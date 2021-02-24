361c6171c31c576db5800be6ff1d664e26805fa0
Abhiek187 <achaudhuri2011@yahoo.com>
2021-02-24 15:43:11 -0500

    Fade between the word and definition

--- stockhelper/stockapp/static/stockapp/js/app.js
+++ stockhelper/stockapp/static/stockapp/js/app.js
@@ -2,6 +2,23 @@ const flashcards = document.querySelectorAll(".flashcard");
 
 for (const card of flashcards) {
     card.addEventListener("click", event => {
-        console.log(`You clicked on ${event.target.textContent}`);
+        // Toggle between viewing the word and the definition
+        const word = card.querySelector(".word");
+        const definition = card.querySelector(".definition");
+
+        if (word.classList.contains("hidden")) {
+            word.classList.add("fade-in");
+            word.classList.remove("fade-out");
+            definition.classList.add("fade-out");
+            definition.classList.remove("fade-in");
+        } else {
+            word.classList.add("fade-out");
+            word.classList.remove("fade-in");
+            definition.classList.add("fade-in");
+            definition.classList.remove("fade-out");
+        }
+
+        word.classList.toggle("hidden");
+        definition.classList.toggle("hidden");
     });
 }
