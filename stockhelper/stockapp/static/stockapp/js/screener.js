4ff33635d4e12b9bc8c4a78b01edc248dd826165
Abhiek187 <achaudhuri2011@yahoo.com>
2021-03-09 18:32:53 -0500

    Add a form to the screener page

---
+++ stockhelper/stockapp/static/stockapp/js/screener.js
@@ -0,0 +1,17 @@
+const stockForm = document.querySelector(".stock-form");
+const regionSelect = document.querySelector(".region-select");
+const priceRelationSelect = document.querySelector(".price-relation-select");
+const priceNumInput = document.querySelector(".price-num-input");
+const sectorSelect = document.querySelector(".sector-select");
+const results = document.querySelector(".results");
+
+stockForm.addEventListener("submit", event => {
+    event.preventDefault(); // don't reload the page
+
+    // Search for stocks based on the given form values
+    const region = regionSelect.value;
+    const priceRelation = priceRelationSelect.value;
+    const priceNum = priceNumInput.value;
+    const sector = sectorSelect.value;
+    results.textContent = `Searching for stocks in the ${region} ${priceRelation} $${priceNum} in the ${sector} sector...`;
+});
