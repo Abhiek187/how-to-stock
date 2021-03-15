d5cbdad01143050bd7ec33091f7f5d9b227434eb
Abhiek187 <achaudhuri2011@yahoo.com>
2021-03-15 19:58:47 -0400

    Simplify the url syntax

--- stockhelper/stockapp/static/stockapp/js/screener.js
+++ stockhelper/stockapp/static/stockapp/js/screener.js
@@ -20,5 +20,5 @@ const tableRows = document.querySelectorAll(".table-row");
 
 for (const row of tableRows) {
     const symbol = row.firstElementChild.textContent;
-    row.onclick = () => window.open(`${window.location.origin}/stockapp/screener/${symbol}`, "_blank", rel="noopener noreferrer");
+    row.onclick = () => window.open(`${window.location.href}/${symbol}`, "_blank", rel="noopener noreferrer");
 }
