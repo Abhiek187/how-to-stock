657904f2787e667456592855d06190dc792e9bdb
Abhiek187 <achaudhuri2011@yahoo.com>
2021-03-17 12:29:19 -0400

    Highlight screener in navbar for details

--- stockhelper/stockapp/static/stockapp/js/home.js
+++ stockhelper/stockapp/static/stockapp/js/home.js
@@ -1,13 +1,19 @@
 const title = document.title;
 
 // Make the clicked link active and remove the active class from the previous link
-if (title !== "How to Stocks" && !title.includes("Details")) {
+if (title !== "How to Stocks") {
     const homeLink = document.querySelector(".link-home");
     homeLink.classList.remove("active");
     homeLink.removeAttribute("aria-current");
+    let activeLink;
 
     // The class name matches the title name, but in lowercase
-    const activeLink = document.querySelector(`.link-${title.toLowerCase()}`);
+    if (title.includes("Details")) {
+        activeLink = document.querySelector(".link-screener");
+    } else {
+        activeLink = document.querySelector(`.link-${title.toLowerCase()}`);
+    }
+
     activeLink.classList.add("active");
     // Tell screen readers this is the current page
     activeLink.setAttribute("aria-current", "page");
