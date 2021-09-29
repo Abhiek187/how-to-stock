6df6eb1136052aca1e7a6e4f69c7466c245125e5
Abhiek187 <achaudhuri2011@yahoo.com>
2021-09-28 22:17:46 -0400

    Don't show errors if the user first clicked on create account

--- stockhelper/stockhelper/views.py
+++ stockhelper/stockhelper/views.py
@@ -1,21 +1,24 @@
-from django.contrib.auth import login, authenticate
-from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect, render
 
 from .forms import CustomUserCreationForm
 
 
 # Create Account view
 def create_account(request):
-    form = CustomUserCreationForm(request.POST)
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
 
-    if form.is_valid():
-        form.save()
-        username = form.cleaned_data.get("username")
-        # password1 and password2 should match
-        password = form.cleaned_data.get("password1")
-        user = authenticate(username=username, password=password)
-        login(request, user)
-        # Upon successful creation of the account, redirect the user to the home page
-        return redirect("/stockapp")
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            # password1 and password2 should match
+            password = form.cleaned_data.get("password1")
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            # Upon successful creation of the account, redirect the user to the home page
+            return redirect("/stockapp")
+    else:
+        form = CustomUserCreationForm()
 
     return render(request, "registration/create.html", {"form": form})
