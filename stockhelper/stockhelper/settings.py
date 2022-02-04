3514d025855442b116e90a14bc75c43d5d701e9e
Abhiek187 <achaudhuri2011@yahoo.com>
2022-02-03 22:56:35 -0500

    Log more detailed errors in prod

--- stockhelper/stockhelper/settings.py
+++ stockhelper/stockhelper/settings.py
@@ -134,6 +134,31 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# Get emails from logs of class AdminEmailHandler
+ADMINS = [("Abhishek Chaudhuri", "achaudhuri2011@yahoo.com")]
+
+# Log all errors in production similar to debug mode
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+            'propagate': False,
+        },
+    },
+}
+
 # Activate Django-on-Heroku (breaks if testing)
 if sys.argv[1:2] != ["test"]:
-    django_on_heroku.settings(locals())
+    django_on_heroku.settings(locals(), logging=False)  # logging=False prevents LOGGING from being overwritten
