9a328d8de7ab9f75e613b12bef134edbbcb25137
Abhiek187 <achaudhuri2011@yahoo.com>
2022-10-21 21:05:51 -0400

    Add comment to explain CSRF_TRUSTED_ORIGINS

--- stockhelper/stockhelper/settings.py
+++ stockhelper/stockhelper/settings.py
@@ -42,6 +42,7 @@ DEBUG = os.environ.get("DEBUG", "false").lower() == "true"
 # Trust localhost during development and Fly.io when deployed
 ALLOWED_HOSTS = [".localhost", "127.0.0.1", "[::1]", "how-to-stock-3.fly.dev"]
 
+# Required for POST requests in prod
 CSRF_TRUSTED_ORIGINS = ["https://how-to-stock-3.fly.dev"]
 SESSION_COOKIE_SAMESITE = "Strict"
 SESSION_COOKIE_SECURE = True
